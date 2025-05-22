460c60f82181c74061fea4b3ba1bb74042044104
Bruno Eduardo <bruno.139737@gmail.com>
2025-05-21 22:31:39 -0300

    Atualiza a configuração do Next.js para alterar o hostname da API de 'api.ghostpisosindustriais.com' para 'ghostpisosindustriais.com'.

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
           }
         : {
             protocol: 'https',
-            hostname: 'api.ghostpisosindustriais.com',
+            hostname: 'ghostpisosindustriais.com',
             pathname: '/**',
           },
     ],
