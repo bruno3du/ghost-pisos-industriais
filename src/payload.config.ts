5b900401029c2c97c2146ee0ab14fcd7ceaf07f2
Bruno Eduardo <bruno.139737@gmail.com>
2025-05-21 23:54:21 -0300

    Adiciona suporte ao armazenamento em S3 no Payload CMS, incluindo configuração de plugins e importações necessárias. Remove imagens desnecessárias do diretório de mídia.

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -2,6 +2,7 @@
 import { mongooseAdapter } from '@payloadcms/db-mongodb';
 import { nodemailerAdapter } from '@payloadcms/email-nodemailer';
 import { lexicalEditor } from '@payloadcms/richtext-lexical';
+import { s3Storage } from '@payloadcms/storage-s3';
 import path from 'path';
 import { buildConfig } from 'payload';
 import sharp from 'sharp';
@@ -45,4 +46,26 @@ export default buildConfig({
       },
     },
   }),
+  plugins: [
+    s3Storage({
+      collections: {
+        media: true,
+      },
+      bucket: process.env.S3_BUCKET || '',
+      config: {
+        credentials: {
+          accessKeyId: process.env.S3_ACCESS_KEY_ID || '',
+          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || '',
+        },
+        region: process.env.S3_REGION || '',
+        endpoint: process.env.S3_ENDPOINT || '',
+        bucketEndpoint: false,
+        endpointProvider: (params) => {
+          return {
+            url: new URL(params.Endpoint as string),
+          }
+        }
+      },
+    }),
+  ],
 });
